860fbd9d8cdaa658062da0da3e6bc107e7df6f44
mtkramer <kramerm1@mail.usf.edu>
2020-07-19 12:22:04 -0400

    removed unnecessary new lines from Employer.java

--- src/org/launchcode/techjobs_oo/Employer.java
+++ src/org/launchcode/techjobs_oo/Employer.java
@@ -2,9 +2,7 @@ package org.launchcode.techjobs_oo;
 
 public class Employer extends JobField {
 
-    public Employer() {
-        super();
-    }
+    public Employer() { super(); }
 
     public Employer(String value) {
         super(value);
